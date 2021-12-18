5aab433f0d079a3c5a3b7abc32d4c34ec9719ceb
nickguerra <nickg@maliciousgames.com>
2021-12-18 08:24:15 -0600

    Initial Commit

---
+++ Recursion/RecursionExamples.cs
@@ -0,0 +1,22 @@
+﻿using Recursion.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursion
+{
+    public class RecursionExamples
+    {
+        public int GetMyPositionInLine(Person person)
+        {
+            if (person.NextInLine == null)
+            {
+                return 1;
+            }
+
+            return 1 + GetMyPositionInLine(person.NextInLine);
+        }
+    }
+}
